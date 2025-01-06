ed53e49e265ea8b35bddf2d4ad4208ee7c199586
dkds <dkds@users.noreply.github.com>
2025-01-06 22:17:55 +0530

    Add card and list components

---
+++ src/components/List.jsx
@@ -0,0 +1,21 @@
+/* eslint-disable react/prop-types */
+
+import Card from "./Card";
+
+export default function List({items}) {
+  return (
+    <div className="bg-white">
+      <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+        <h2 className="sr-only">Products</h2>
+
+        <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+          {items.map((product) => (
+            <Card>
+              
+            </Card>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
