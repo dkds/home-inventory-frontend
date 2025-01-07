5cc89f7a88aa3c38aa5739034f3e0b93bbc1df83
dkds <dkds@users.noreply.github.com>
2025-01-07 19:50:37 +0530

    Split components on Dashboard

---
+++ src/components/ProfileMenu.jsx
@@ -0,0 +1,31 @@
+/* eslint-disable react/prop-types */
+import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
+
+export default function ProfileMenu({ user, userNavigation }) {
+  return (
+    <Menu as="div" className="relative ml-3">
+      <div>
+        <MenuButton className="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+          <span className="absolute -inset-1.5" />
+          <span className="sr-only">Open user menu</span>
+          <img alt="" src={user.imageUrl} className="size-8 rounded-full" />
+        </MenuButton>
+      </div>
+      <MenuItems
+        transition
+        className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black/5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
+      >
+        {userNavigation.map((item) => (
+          <MenuItem key={item.name}>
+            <a
+              href={item.href}
+              className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:outline-none"
+            >
+              {item.name}
+            </a>
+          </MenuItem>
+        ))}
+      </MenuItems>
+    </Menu>
+  );
+}
---
+++ src/components/ProfileMenuMobile.jsx
@@ -0,0 +1,30 @@
+/* eslint-disable react/prop-types */
+import { DisclosureButton } from "@headlessui/react";
+
+export default function ProfileMenuMobile({ user, userNavigation }) {
+  return (
+    <div className="border-t border-gray-700 pb-3 pt-4">
+      <div className="flex items-center px-5">
+        <div className="shrink-0">
+          <img alt="" src={user.imageUrl} className="size-10 rounded-full" />
+        </div>
+        <div className="ml-3">
+          <div className="text-base/5 font-medium text-white">{user.name}</div>
+          <div className="text-sm font-medium text-gray-400">{user.email}</div>
+        </div>
+      </div>
+      <div className="mt-3 space-y-1 px-2">
+        {userNavigation.map((item) => (
+          <DisclosureButton
+            key={item.name}
+            as="a"
+            href={item.href}
+            className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
+          >
+            {item.name}
+          </DisclosureButton>
+        ))}
+      </div>
+    </div>
+  );
+}
