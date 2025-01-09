c7b4e728e278d35fddf4d8078482eddae3d2ec94
dkds <dkds@users.noreply.github.com>
2025-01-09 23:27:32 +0530

    Update List to support custom list items

--- src/components/ContainerList.jsx
+++ src/components/ContainerList.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { listTopContainers } from "../services/container.service";
 import Header from "./Header";
 import List from "./List";
+import Card from "./Card";
 
 export default function ContainerList() {
   const [containers, setContainers] = useState([]);
@@ -21,7 +22,18 @@ export default function ContainerList() {
       <Header title="Containers" />
       <main>
         <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-          <List items={containers} />
+          <List
+            items={containers}
+            listItem={(item) => (
+              <Card key={item.id} title={item.name}>
+                <p>
+                  Containers:
+                  <br />
+                  Items:
+                </p>
+              </Card>
+            )}
+          />
         </div>
       </main>
     </>
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,13 +1,9 @@
 /* eslint-disable react/prop-types */
 
-import Card from "./Card";
-
-export default function List({ items }) {
+export default function List({ items, listItem }) {
   return (
     <div className="grid grid-cols-1 gap-x-6 gap-y-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-      {items.map((product) => (
-        <Card key={product.id}>{product.name}</Card>
-      ))}
+      {items.map((item) => listItem(item))}
     </div>
   );
 }
