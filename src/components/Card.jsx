7c6563d3a20b257ed906ec551d00df17f49077f6
dkds <dkds@users.noreply.github.com>
2025-01-14 23:52:19 +0530

    Refactor Card component to support images

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,20 +1,28 @@
 /* eslint-disable react/prop-types */
-export default function Card({ title, children, actions, className, onClick }) {
-  const classes = `max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 ${className}`;
+export default function Card({
+  title,
+  children,
+  actions,
+  image,
+  className,
+  onClick,
+}) {
+  const classes = `max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 ${className}`;
   return (
     <div className={classes} onClick={onClick}>
+      {image && <img className="rounded-t-lg pb-1" src={image} alt="" />}
       {title && (
-        <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+        <h5 className="mb-2 px-4 py-1 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
           {title}
         </h5>
       )}
       {children && (
-        <div className="font-normal text-gray-700 dark:text-gray-400">
+        <div className="font-normal px-4 py-2 text-gray-700 dark:text-gray-400">
           {children}
         </div>
       )}
       {actions && (
-        <div className="inline-flex items-center px-3 py-2 mt-3  text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        <div className="inline-flex p-4 items-center px-3 py-2 mt-3  text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
           {actions}
         </div>
       )}
--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -8,7 +8,7 @@ import Card from "./Card";
 
 export default function ItemList() {
   const dispatch = useDispatch();
-  const containers = useSelector((state) => state.container.containers);
+  const items = useSelector((state) => state.container.containers);
 
   useEffect(() => {
     dispatch(loadContainers());
@@ -22,21 +22,22 @@ export default function ItemList() {
     <>
       <div className="mx-auto max-w-7xl px-4 py-3 sm:px-6 lg:px-8">
         <div className="grid grid-cols-1 gap-x-6 gap-y-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-          {containers.map((container) => (
+          {items.map((item) => (
             <Card
-              key={container.id}
-              title={container.name}
+              key={item.id}
+              title={item.name}
               className="cursor-pointer"
-              onClick={() => handleCardClick(container.id)}
+              image="https://fastly.picsum.photos/id/11/2500/1667.jpg?hmac=xxjFJtAPgshYkysU_aqx2sZir-kIOjNR9vx0te7GycQ"
+              onClick={() => handleCardClick(item.id)}
             >
               <p>
-                Containers: {container.childContainerCount}
+                Containers: {item.childContainerCount}
                 <br />
-                Items: {container.itemCount}
+                Items: {item.itemCount}
               </p>
             </Card>
           ))}
-          {!containers.length && <p>No items found</p> }
+          {!items.length && <p>No items found</p> }
         </div>
       </div>
     </>
